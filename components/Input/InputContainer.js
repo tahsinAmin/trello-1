337a1b91b84b0f26e8e2eadcba0f4f2a714a5cd6
tahsinAmin <muhammadtahsinamin@gmail.com>
2022-05-26 23:28:08 +0600

    Power Move

--- components/Input/InputContainer.js
+++ components/Input/InputContainer.js
@@ -10,7 +10,8 @@ export default function InputContainer({ listId, type }) {
          <Collapse in={isOpen} animateOpacity>
             <InputCard listId={listId} onClose={onClose} type={type} />
          </Collapse>
-         <div className='p-1 m-1 mt-0 bg-[#ebecf0] hover:bg-gray-300 transition duration-200 cursor-pointer'>
+         {/* Power Move 1 */}
+         <div className={`${!isOpen ? 'p-1' : ''} m-1 mt-0 bg-[#ebecf0] hover:bg-gray-300 transition duration-200 cursor-pointer`}>
             {!isOpen && <p onClick={onOpen}
                className='text-gray-500'>  {`+ Add ${type === 'Card' ? "a Card" : 'another List'}`} </p>
             }
